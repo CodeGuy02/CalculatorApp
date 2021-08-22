7c2734a47e99c7230668226577b4570e58cbc1c4
codeguy02 <codeguy02@example.com>
2021-08-22 11:28:43 -0400

    lots of additions to CSS, added basic functionality, images

---
+++ calculator.js
@@ -0,0 +1,182 @@
+var numbers = [];
+
+var operatorCount = 0;
+
+function buttonPress(ourNum) {
+    var otherOperatorPresent = false;
+    var otherOperator = '';
+
+    if (typeof(ourNum) == 'number') {
+        newOutput = document.getElementById('outputText').innerHTML + ourNum;
+        document.getElementById('outputText').innerHTML = newOutput;
+    } else
+    if (typeof(ourNum) == 'string') {
+        var operatorPresent = false;
+        var lastOperator = '';
+        
+        if ((ourNum === '+') || (ourNum === '-') || (ourNum === 'x') || (ourNum === '/')) {
+            operatorCount += 1;
+            operatorPresent = true;
+            //alert('operatorCount = ' + operatorCount + ' symbol:' + ourNum);
+        }
+
+        if (operatorPresent && operatorCount > 1) {
+            //alert('operatorCount = ' + operatorCount + ' symbol:' + ourNum + ' > 1');
+            
+            // check for other operators in string:
+
+            var workingString = document.getElementById('outputText').innerHTML;
+            workingString = document.getElementById('outputText').innerHTML = workingString + ourNum;
+            //workingString.concat(ourNum);
+            // remove last operator from string
+            lastOperator = workingString.slice(workingString.length - 1, workingString.length);
+            currentString = workingString.slice(0, workingString.length-1);
+            // check if other operators are present:
+
+            if (currentString.indexOf('+') != -1) {
+                otherOperatorPresent = true;
+                otherOperator = '+';
+            } else
+            if (currentString.indexOf('-') != -1) {
+                otherOperatorPresent = true;
+                otherOperator = '-';
+            } else
+            if (currentString.indexOf('x') != -1) {
+                otherOperatorPresent = true;
+                otherOperator = 'x';
+            } else
+            if (currentString.indexOf('/') != -1) {
+                otherOperatorPresent = true;
+                otherOperator = '/';
+            }
+
+            // if more than one operator exists, immediately calculate using first operator
+            if (otherOperatorPresent) {
+                var total = 0;
+                //alert('another operator present: ' + otherOperator);
+                if (otherOperator == '+') {
+                    const ourNumbers = currentString.split('+');
+
+                    
+                    ourNumbers.forEach(function (item, index) {
+                        total += parseInt(item);
+                    });
+
+                    operatorCount--;
+                } else
+                if (otherOperator == '-') {
+                    const ourNumbers = currentString.split('-');
+
+                    
+                    ourNumbers.forEach(function (item, index) {
+                        total -= parseInt(item);
+                    });
+
+                    operatorCount--;
+                }
+                //alert('last operator: ' + lastOperator);
+                document.getElementById('outputText').innerHTML = total + lastOperator;
+            }
+
+            // return new string with last operator:
+        } else
+
+        if (operatorPresent && operatorCount == 1) {
+            newOutput = document.getElementById('outputText').innerHTML + ourNum;
+            document.getElementById('outputText').innerHTML = newOutput;
+        } else
+
+        if (ourNum === '=') {
+            //alert('lastOperator:  ' + lastOperator);
+            //alert('otherOperator: ' + otherOperator);
+            entireString = document.getElementById('outputText').innerHTML;
+            //alert(entireString);
+
+            if (entireString.indexOf('+') != -1) {
+                const ourNumbers = entireString.split('+');
+
+                var total = 0;
+                ourNumbers.forEach(function (item, index) {
+                    total += parseInt(item);
+                });
+            } else
+            if (entireString.indexOf('-') != -1) {
+                const ourNumbers = entireString.split('-');
+
+                var total = 0;
+                firstNum = 0;
+                secondNum = 0;
+                // foreach() uses zero index for first element
+                ourNumbers.forEach(function (item, index) {
+                    if (index == 0) {
+                        firstNum = item;
+                    } else
+                    if (index == 1) {
+                        secondNum = item; 
+                    }
+                })
+                total = firstNum - secondNum;
+            } else
+            if (entireString.indexOf('x') != -1) {
+                const ourNumbers = entireString.split('x');
+
+                var total = 0;
+                firstNum = 0;
+                secondNum = 0;
+                // foreach() uses zero index for first element
+                ourNumbers.forEach(function (item, index) {
+                    if (index == 0) {
+                        firstNum = item;
+                    } else
+                    if (index == 1) {
+                        secondNum = item; 
+                    }
+                })
+                total = firstNum * secondNum;
+            } else
+            if (entireString.indexOf('/') != -1) {
+                const ourNumbers = entireString.split('/');
+
+                var total = 0;
+                firstNum = 0;
+                secondNum = 0;
+                // foreach() uses zero index for first element
+                ourNumbers.forEach(function (item, index) {
+                    if (index == 0) {
+                        firstNum = item;
+                    } else
+                    if (index == 1) {
+                        secondNum = item; 
+                    }
+                })
+                total = firstNum / secondNum;
+            }
+
+
+            document.getElementById('outputText').innerHTML = total;
+
+            operatorCount = 0;
+        }
+    }
+}
+
+function clearOutput() {
+    document.getElementById('outputText').innerHTML = '';
+    operatorCount = 0;
+    //alert('operator count = ' + operatorCount);
+}
+
+function clearLastEntry() {
+    workingString = document.getElementById('outputText').innerHTML;
+
+    // Remove last character using slice()
+    lastCharacter = workingString.slice(workingString.length - 1, workingString.length);
+    newString = workingString.slice(0, workingString.length - 1);
+    document.getElementById('outputText').innerHTML = newString;
+
+    if (lastCharacter === '+') {
+        operatorCount--;
+        //alert('operator count = ' + operatorCount);
+    }
+}
+
